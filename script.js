6c060497ece94e277cfb723d79424088af95702b
kkm6418 <pink_odette@yahoo.com>
2026-07-19 11:39:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -290,7 +290,7 @@ function getDailyLaw() {
 function displayLaw(law) {
     document.getElementById("law-title").textContent = law.title;
     document.getElementById("law-text").textContent = law.text;
-    document.getElementById("challenge").textContent = law.challenge;
+    document.getElementById("challenge").textContent = law.reflection;
 }
 
 displayLaw(getDailyLaw());
